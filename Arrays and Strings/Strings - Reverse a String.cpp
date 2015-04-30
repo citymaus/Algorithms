73bf401c6f3687474ca6cf468b4f0ab758c87947
citymaus <citymaus@users.noreply.github.com>
2015-04-29 22:15:45 -0400

    other methods

--- Arrays and Strings/Strings - Reverse a String.cpp
+++ Arrays and Strings/Strings - Reverse a String.cpp
@@ -16,3 +16,35 @@ void reverseString()
  
   cout << "Reverse of entered string is: " << reverse;
 }
+
+void strrev(char *p)
+{
+  char *q = p;
+  while(q && *q) ++q;
+  for(--q; p < q; ++p, --q)
+    *p = *p ^ *q,
+    *q = *p ^ *q,
+    *p = *p ^ *q;
+}
+
+/* PRE: str must be either NULL or a pointer to a 
+ * (possibly empty) null-terminated string. */
+void strrev(char *str) 
+{
+  char temp, *end_ptr;
+
+  /* If str is NULL or empty, do nothing */
+  if( str == NULL || !(*str) )
+    return;
+
+  end_ptr = str + strlen(str) - 1;
+
+  /* Swap the chars */
+  while( end_ptr > str ) {
+    temp = *str;
+    *str = *end_ptr;
+    *end_ptr = temp;
+    str++;
+    end_ptr--;
+  }
+}
