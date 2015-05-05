7db962ef91d943235406d61f9040653973cba010
citymaus <citymaus@users.noreply.github.com>
2015-05-05 00:08:07 -0400

    update

--- Arrays and Strings/Strings - Reverse a String.cpp
+++ Arrays and Strings/Strings - Reverse a String.cpp
@@ -1,6 +1,10 @@
 /*----------------------
   Reverse a string
 ------------------------*/
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
 
 void reverseString()
 {
@@ -9,7 +13,7 @@ void reverseString()
   cout << "Enter a string to reverse" << endl;
   getline(cin, original);
  
-  int length = strlen(original);
+  int length = original.length();
  
   for (int i = length - 1; i >= 0 ; i--)
       reverse = reverse + original[i];
