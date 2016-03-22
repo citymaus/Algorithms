db03ffb3709f8dd9f52de6c3c4c31b7eb0d926a8
citymaus <citymaus@users.noreply.github.com>
2016-03-22 01:27:52 -0400

    update
    comments

--- Arrays and Strings/Strings - Find First Non-repeating Character.cpp
+++ Arrays and Strings/Strings - Find First Non-repeating Character.cpp
@@ -11,7 +11,8 @@ string firstNonRepeatingCharacter(string input) {
     string nonrepeating = "";
     stringstream ss;
     map<char, int>::iterator it;
-    
+
+    // aaaabbcddd    
     for (int i = 0; i < input.length(); i++)
     {
         if (input[i] != ' ')
@@ -19,15 +20,24 @@ string firstNonRepeatingCharacter(string input) {
             it = freq.find(input[i]); 
             if (it != freq.end())
             {
+                // Character already exists
                 if (it->second == 1)
                 {
-                    newchars.pop();
+                    // Pop front of the queue, first entered character
+                    //while(newchars.front() != input[i])
+                    //{
+                      //char temp = newchars.front();
+                      newchars.pop();
+                    //}
                 }
+                // Add to frequency char appears
                 freq[input[i]] += 1;
             }
             else
             {
+                // Add character to the back of the queue
                 newchars.push(input[i]);
+                // Add character frequency to map, start at 1
                 freq.insert(pair<char, int> (input[i], 1));
             }
         }
