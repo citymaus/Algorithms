65d3280271e073f456df0a6b5570cf786a30b224
citymaus <citymaus@users.noreply.github.com>
2016-03-24 21:55:30 -0400

    add
    new

---
+++ Linked Lists/Find_Nth_Node_from_End.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+/*
+Find nth node from the end of a Linked List
+Given a Linked List and a number n, write a function that returns the value 
+at the nth node from end of the Linked List.
+*/
+
+struct node
+{
+  int data;
+  struct node* next;
+};
+ 
+/* Function to get the nth node from the last of a linked list*/
+void printNthFromLast(struct node *head, int n)
+{
+  struct node *main_ptr = head;
+  struct node *ref_ptr = head;
+ 
+  int count = 0;
+  if(head != NULL)
+  {
+     while( count < n )
+     {
+        if(ref_ptr == NULL)
+        {
+           printf("%d is greater than the no. of "
+                    "nodes in list", n);
+           return;
+        }
+        ref_ptr = ref_ptr->next;
+        count++;
+     } /* End of while*/
+ 
+     while(ref_ptr != NULL)
+     {
+        main_ptr = main_ptr->next;
+        ref_ptr  = ref_ptr->next;
+     }
+     printf("Node no. %d from last is %d ", 
+              n, main_ptr->data);
+  }
+}
